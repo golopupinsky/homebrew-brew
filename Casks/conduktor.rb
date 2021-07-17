87d78fddf16360c3314062c5156545b1993e8f9b
golopupinsky <golopupinsky@gmail.com>
2021-07-17 17:20:46 +0000

    Version bump

--- Casks/conduktor.rb
+++ Casks/conduktor.rb
@@ -1,6 +1,6 @@
 cask "conduktor" do
-  version "2.16.1"
-  sha256 "b8bc5051529dd5f7a6ab0cd2811f0af896d50ecfd5adac4c829ff203a79e20d5"
+  version "2.16"
+  sha256 "545f62c0c3258121f632b175dc0d1641f2a47a3d24d2d593512f9cbfbfd9ce4c"
 
   url "https://github.com/conduktor/builds/releases/download/v#{version}/Conduktor-#{version}.pkg", verified: "https://github.com/conduktor"
   name "conduktor"
